c0d03ea3f44ed37cd80fd44ed0b7ad3b63dca767
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-28 16:17:30 -0400

    Launched Empty Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenStackAPIDemo::Application.config.secret_token = '5d1eeaa4d3a2dae01f4bf2fa82a4bf2e1ec41a373aa07b69a4ce8874d1172ac4839cacd29b234c24bfcafec9d75c16355b604edb4508a3b3d211981614bdd0a1'
